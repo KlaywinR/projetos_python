2badba0e05eeafee56506b170dd5aa013fa96db8
Klaywin Ryan <klaywindias@gmail.com>
2025-08-28 21:51:45 -0300

    Create ex003.py

---
+++ IMERSÃO DIO/exercícios/ex003.py
@@ -0,0 +1,14 @@
+#CRIE UM PROGRAMA QUE FACA A MÉDIA DE INSUMOS AGRICOLAS DE 4 REGIOES DO BRASIL:
+
+regiao_sul = 108.000
+regiao_centro_oeste = 22.000
+regiao_nordeste = 39.0000
+regiao_norte = 12.000
+
+valor_totall = regiao_sul + regiao_norte + regiao_centro_oeste + regiao_nordeste 
+media_fnal = regiao_sul + regiao_norte + regiao_centro_oeste + regiao_nordeste / 4
+
+print(f"O VALOR TOTAL DE INSUMOS PRODUZIDOS NAS 4 REGIOES EH: {valor_totall}")
+
+print(f"A MEDIA DAS 3 REGIOES EH: {media_fnal}")
+ 
