1a432ee9ed101aa93b919e4d464262b77514af3b
Klaywin Ryan <klaywindias@gmail.com>
2025-08-28 21:51:55 -0300

    Create ex02.py

---
+++ IMERSÃO DIO/exercícios/ex02.py
@@ -0,0 +1,8 @@
+#CRIE UM PROGRAMA QUE CALCULA O VALOR APÓS UMA TAXA DE DESCONTO:
+porcentagem_de_desconto = 10
+valor_total = 1.800
+
+desconto_percentual = (porcentagem_de_desconto / 100) * valor_total
+
+valor_final = valor_total - desconto_percentual
+print(f"O VALOR FINAL EH DE: {valor_final}")
