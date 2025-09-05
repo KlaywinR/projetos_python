0d944d8b68e5a5b80058d8e30ea26f0625bb6416
Klaywin Ryan <klaywindias@gmail.com>
2025-09-04 21:30:36 -0300

    Create ex001.py

---
+++ PROJETOS/scripts/ex001.py
@@ -0,0 +1,7 @@
+##crie um script de perguntas e respostas simples!
+
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+altura_pessoa = input('Qual a sua altura?') 
+
+print(f"Olá(a),{nome}!\n",f"Voce possui{idade} anos\n", f"Possui{altura_pessoa} metros de altura.\n")
