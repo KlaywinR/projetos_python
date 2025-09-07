5ad0203d0c11f7ebd18d53a3dba9e3b5522aaba1
Klaywin Ryan <klaywindias@gmail.com>
2025-09-07 11:57:01 -0300

    Create tipo.py

---
+++ PROJETOS/scripts/tipo.py
@@ -0,0 +1,8 @@
+##descobrindo a tipagem primitiva de numeros aleatórios:
+n1 = int(input('digite o primeiro numero:'))
+n2 = int(input('digite o segundo numero:'))
+
+print(type(n1))
+print(type(n2))
+
+22
