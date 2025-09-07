3a2488d6656b8aad7bd5f0b4ee447e4729b4b982
Klaywin Ryan <klaywindias@gmail.com>
2025-09-07 11:56:43 -0300

    Create soma.py

---
+++ PROJETOS/scripts/soma.py
@@ -0,0 +1,9 @@
+##SOMANDO A ALTURA DE TRÊS PESSOAS DE UMA DETERMINADA FAMILIA 
+
+numero01 = float(input('Digite a primeira altura da pessoa para a sua soma: '))
+numero02 = float(input('Digite a segunda altura da pessoa para a sua soma: '))
+numero03 = float(input('Digite a terceira altura da pessoa para a sua soma: '))
+
+soma_final = numero01 + numero02 + numero03
+
+print('SUA SOMA EH {}'.format(soma_final))
