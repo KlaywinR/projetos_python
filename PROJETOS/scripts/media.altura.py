773052e0e9322fee983b83075432fc57c2a4a23e
Klaywin Ryan <klaywindias@gmail.com>
2025-09-07 11:56:32 -0300

    Create media.altura.py

---
+++ PROJETOS/scripts/media.altura.py
@@ -0,0 +1,10 @@
+##Programa desenvolvido para descobrir a media de alturas de pessoas de uma determinada familia
+pessoa01 = float(input('Digite a altura da primeira pessoa: '))
+pessoa02 = float(input('Digite a altura da segunda pessoa: '))
+pessoa03 = float(input('Digite a altura da terceira pessoa: '))
+pessoa04 = float(input('Digite a altura da quarta pessoa: '))
+
+media_alt = (pessoa01 + pessoa02 + pessoa03 + pessoa04) / 4
+
+print("A media de alturas é equivalente a:{}".format(media_alt))
+ 
