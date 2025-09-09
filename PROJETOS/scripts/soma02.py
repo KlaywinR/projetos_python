71c1f86a43adc52b033aeabc33fbdcfe413ffe00
Klaywin Ryan <klaywindias@gmail.com>
2025-09-08 21:43:02 -0300

    Create soma02.py

---
+++ PROJETOS/scripts/soma02.py
@@ -0,0 +1,6 @@
+print("DIGITE DOIS NUMEROS PARA UMA SOMA SIMPLES:")
+soma01 = int(input('digite um numero inteiro:'))
+soma02 = int(input('digite outro numero inteiro: '))
+
+print("A soma e: {}".format(soma01+soma02))
+
