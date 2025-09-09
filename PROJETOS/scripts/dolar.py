dd63a71d1fdfb0b54db112caff8f230efd8068c6
Klaywin Ryan <klaywindias@gmail.com>
2025-09-08 21:43:12 -0300

    Create dolar.py

---
+++ PROJETOS/scripts/dolar.py
@@ -0,0 +1,7 @@
+## crie um programa que conte quantos X de reais ela possui na carteira e mostre a quantidadev de dólares que ela pode comprar!
+
+dinheiro = int(input('Digite a quantidade de dinheiro que você possui na sua carteira: '))
+print("\t----------------------------------------------------\t")
+print("O valor que você possui é dado por: R$ {} ".format(dinheiro))
+print('A quantidade de dólares disponíveis para você é: {}'.format(dinheiro / 3,27 ))
+print("Cotação Atual do dólar: U$ 3,27")
